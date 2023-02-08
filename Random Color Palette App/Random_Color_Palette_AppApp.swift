53ac9b15370c1e80d2278d589697e26341d2a762
adiamoyal <adi.amoyal02@gmail.com>
2023-02-08 21:53:47 +0200

    Initial Commit

---
+++ Random Color Palette App/Random_Color_Palette_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Color_Palette_AppApp.swift
+//  Random Color Palette App
+//
+//  Created by Adi Amoyal on 08/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Random_Color_Palette_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
